5431d4e559104f139e3ec71e01482f78176166d2
Nick Jamil <nickjamil@hotmail.com>
2020-04-10 10:07:16 -0400

    Create a ToDo API from the Asp.Net Core 3.1 tutorial

---
+++ ToDoApi/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+    }
+}
